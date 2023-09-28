b48ffecdf048488bcd3f58856f1ff5ff954c49b3
amandajones1996 <jonesamanda996@gmail.com>
2023-09-28 15:24:06 -0700

    phrase class methods

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay(){
+        const phraseSection = document.querySelector('#phrase ul');
+        const phraseLetters = this.phrase.split('');
+
+        phraseLetters.forEach(letter => {
+            const li = document.createElement('li');
+            if (letter === ' '){
+                li.className = 'space';
+            } else {
+                li.className = `hide letter ${letter}`;
+                li.textContent = letter;
+            }
+            phraseSection.appendChild(li);
+        });
+    }
+
+    checkLetter(letter){
+        return this.phrase.includes(letter)
+    }
+
+    showMatchedLetter(letter){
+        const lettersPicked = document.querySelectorAll(`.letter.${letter}`);
+        lettersPicked.forEach(letterPicked => {
+            letterPicked.classList.replace('hide', 'show');
+        })
+    }
+}
